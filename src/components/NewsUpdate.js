5d8dbfd93b4a521d9e3beea1ef19e66d065749b2
Gaurav Mishra <1997gauravmishra@gmail.com>
2023-12-06 23:39:10 +0530

    update

--- src/components/NewsUpdate.js
+++ src/components/NewsUpdate.js
@@ -8,7 +8,7 @@ export class  NewsUpdate extends Component {
 <div className='my-3'>
 
 <div className="card" style={{width: "18rem" }}>
-  <img src="imageUrl" className="card-img-top" alt="..."height="200px"/>
+  <img src={imageUrl} className="card-img-top" alt="..."height="200px"/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}...</p>
